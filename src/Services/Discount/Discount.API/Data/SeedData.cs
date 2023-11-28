5f63257c80c68f46ab4282c85a98b592a03326fd
a.norouzian <a.norouzian@hoodadtech.com>
2023-11-28 10:49:59 +0330

    Discount service added

---
+++ src/Services/Discount/Discount.API/Data/SeedData.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+using Discount.API.Entities;
+using Discount.API.Repositories;
+using Npgsql;
+
+namespace Discount.API.Data
+{
+    public static class SeedData
+    {
+        public static async Task<WebApplication> SeedCouponData(this WebApplication app, WebApplicationBuilder builder, int retryCount = 0)
+        {
+            // ReTry it 10 time
+            if (10 < retryCount)
+                throw new Exception("Database connection has problem!");
+
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            await using var scope = serviceProvider.CreateAsyncScope();
+            var _discountRepository = scope.ServiceProvider.GetService<IDiscountRepository>();
+            var _configuration = scope.ServiceProvider.GetService<IConfiguration>();
+            var _connectionString = _configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            await using var connection = new NpgsqlConnection(_connectionString);
+
+            var data = new List<Coupon>()
+            {
+                new Coupon()
+                {
+                    ProductName = "Samsung 10",
+                    Description = "Samsung Discount",
+                    Amount =100 ,
+                },
+                new Coupon()
+                {
+                    ProductName = "IPhone X",
+                    Description = "IPhone Discount",
+                    Amount =150 ,
+                }
+            };
+
+            try
+            {
+                var query = @"SELECT COUNT(*) 
+                              FROM Coupon;";
+                var couponCount = await connection.ExecuteScalarAsync<int>(query);
+                if (couponCount == 0)
+                    foreach (var coupon in data)
+                        await _discountRepository.CreateDiscount(coupon);
+            }
+            catch (NpgsqlException e)
+            {
+                try
+                {
+                    var query = @"CREATE TABLE Coupon(
+                      ID SERIAL PRIMARY KEY       NOT NULL,
+                      ProductName     VARCHAR(24) NOT NULL,
+                      Description     TEXT,
+                      Amount          INT); ";
+                    await connection.ExecuteAsync(query);
+                    await app.SeedCouponData(builder, retryCount + 1);
+                }
+                catch (NpgsqlException ex)
+                {
+                    // ReTry it 10 time
+                    await app.SeedCouponData(builder, retryCount + 1);
+                }
+            }
+
+            return app;
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,85 @@
+﻿using Dapper;
+using Discount.API.Entities;
+using Npgsql;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private const string CouponTableName = "Coupon";
+        private readonly IConfiguration _configuration;
+        private readonly string ConnectionString;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ConnectionString = _configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            await using var connection = new NpgsqlConnection(ConnectionString);
+
+            var query = @$"SELECT * 
+                        FROM {CouponTableName} 
+                        WHERE ProductName = @ProductName";
+            var parameters = new { ProductName = productName };
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(query, parameters);
+            if (coupon == null)
+                return new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            return coupon;
+            // The connection is automatically disposed at the end of the using block
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            await using var connection = new NpgsqlConnection(ConnectionString);
+
+            var query = @$"INSERT INTO {CouponTableName} 
+                        (ProductName, Description, Amount)
+                        VALUES (@ProductName, @Description, @Amount)";
+            var parameters = coupon;
+
+            var affected = await connection.ExecuteAsync(query, parameters);
+            if (affected == 0)
+                return false;
+
+            return true;
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            await using var connection = new NpgsqlConnection(ConnectionString);
+
+            var query = @$"UPDATE {CouponTableName} 
+                        SET ProductName=@ProductName,
+                            Description=@Description, 
+                            Amount=@Amount
+                        WHERE Id = @Id ";
+            var parameters = coupon;
+
+            var affected = await connection.ExecuteAsync(query, parameters);
+            if (affected == 0)
+                return false;
+
+            return true;
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            await using var connection = new NpgsqlConnection(ConnectionString);
+
+            var query = @$"DELETE FROM {CouponTableName} 
+                        WHERE ProductName=@ProductName ";
+            var parameters = new { ProductName = productName };
+
+            var affected = await connection.ExecuteAsync(query, parameters);
+            if (affected == 0)
+                return false;
+
+            return true;
+        }
+    }
+}
