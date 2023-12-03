d4cc3947c186fe4f5f908174d39cadba8c2e2f8e
a.norouzian <a.norouzian@hoodadtech.com>
2023-12-03 12:55:15 +0330

    Caching added to gateway

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,10 +1,12 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot(builder.Configuration)
+	.AddCacheManager(x => x.WithDictionaryHandle());
 
 var app = builder.Build();
 
