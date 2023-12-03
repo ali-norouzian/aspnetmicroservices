27761c41c09fab1144fcbe9468056becce12b2e2
a.norouzian <a.norouzian@hoodadtech.com>
2023-12-03 11:42:00 +0330

    Gateway Service added with ocdelot

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
